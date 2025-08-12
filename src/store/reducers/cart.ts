6f027ae1dd3518057724e4356af747fe46ac16f7
LUAN FERREIRA <130109744+luanlnf@users.noreply.github.com>
2025-08-12 19:11:06 -0300

    ajustes build

--- src/store/reducers/cart.ts
+++ src/store/reducers/cart.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
-import { Restaurante, Cardapio } from '../../models/restaurante'
+import { Cardapio } from '../../models/restaurante'
 
 type CartState = {
   items: Cardapio[]
